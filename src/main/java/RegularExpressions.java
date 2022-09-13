8c9692f80259ac2df87fe01d55425d05fcfc44e5
NovikovMD <matvey393f@mail.ru>
2022-09-13 19:13:36 +0300

    Another commit
    /*Added Task1 method

--- src/main/java/RegularExpressions.java
+++ src/main/java/RegularExpressions.java
@@ -1,3 +1,13 @@
+import java.util.regex.Pattern;
+
 public class RegularExpressions {
+
+    public static boolean Task1(String s){
+        String pat = "^(25[0-5]|[01]?[0-9]?[0-9]|2[0-4][0-9])\\." +
+                "(25[0-5]|[01]?[0-9]?[0-9]|2[0-4][0-9])\\." +
+                "(25[0-5]|[01]?[0-9]?[0-9]|2[0-4][0-9])\\." +
+                "(25[0-5]|[01]?[0-9]?[0-9]|2[0-4][0-9])$";
+        return s.matches(pat);
+    }
 
 }
