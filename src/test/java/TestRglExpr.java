b2c9ccc546d864b3927b81f05572c030012b041d
NovikovMD <matvey393f@mail.ru>
2022-09-17 16:42:05 +0300

    New commit
    /* Added new tests for Task1

--- src/test/java/TestRglExpr.java
+++ src/test/java/TestRglExpr.java
@@ -26,5 +26,29 @@ public class TestRglExpr {
     public void Task1_6(){
         Assert.assertTrue(RegularExpressions.Task1("9.59.180.1"));
     }
+    @Test
+    public void Task1_1Wrong(){
+        Assert.assertFalse(RegularExpressions.Task1("256.59.180.1"));
+    }
+    @Test
+    public void Task1_2Wrong(){
+        Assert.assertFalse(RegularExpressions.Task1("abd.59.180.1"));
+    }
+    @Test
+    public void Task1_3Wrong(){
+        Assert.assertFalse(RegularExpressions.Task1("9.59.180.1."));
+    }
+    @Test
+    public void Task1_4Wrong(){
+        Assert.assertFalse(RegularExpressions.Task1("9,59,180,1"));
+    }
+    @Test
+    public void Task1_5Wrong(){
+        Assert.assertFalse(RegularExpressions.Task1("255"));
+    }
+    @Test
+    public void Task1_6Wrong(){
+        Assert.assertFalse(RegularExpressions.Task1("Some random text, 255.255.255.255"));
+    }
 
 }
